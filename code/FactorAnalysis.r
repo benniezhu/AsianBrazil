e2fad4249f44ecdea00719b7c6b36d6e3d6ace8d
benniezhu <zhu.jieming.benjamin@gmail.com>
2020-04-11 21:31:07 -0400

    add code for KMO and alpha

--- code/FactorAnalysis.r
+++ code/FactorAnalysis.r
@@ -13,6 +13,8 @@ COPI_score = factor.scores(COPI_Vars, COPI_fit)
 COPI_score = COPI_score$scores
 Brazil = cbind(Brazil2017_2019WorkingCopy, COPI_score)
 mean(na.omit(COPI_score))
+KMO(COPI_Vars)
+psych::alpha(COPI_Vars)
 
 CI_Vars = cbind(b1, b2, b3,b4, b6, b12, b13, b21, b21a, b32, b47a)
 CI_fit = fa(CI_Vars, 1)
@@ -20,6 +22,8 @@ CI_score = factor.scores(CI_Vars, CI_fit)
 CI_score= CI_score$scores
 Brazil2=cbind(Brazil, CI_score)
 mean(na.omit(CI_score))
+KMO(CI_Vars)
+psych::alpha(CI_Vars)
 
 library(foreign)
 write_dta(Brazil2, "C:/Users/Ben/Google Drive/AsianBrazil/data/Brazil2017_2019.dta")
